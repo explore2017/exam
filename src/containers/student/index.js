3eec6be77d02087e0b83781d110e5be8b465a207
WORXFR <3475296507@qq.com>
2019-04-09 20:39:44 +0800

    添加了查看个人班级的功能

--- src/containers/student/index.js
+++ src/containers/student/index.js
@@ -5,6 +5,7 @@ const MenuItemGroup = Menu.ItemGroup;
 //路由组件
 import { Route,Link,Switch  } from 'react-router-dom';
 import 	HeaderBar from './header_bar/index.js';
+import MyClass from './my_class'
 import * as URL from '@components/interfaceURL.js'
 import { get } from '@components/axios.js';
 
@@ -19,11 +20,11 @@ class Student extends React.Component {
 
 	componentWillMount(){
 		//判断用户是否已经登录
-		if(!localStorage.getItem("userName")||!localStorage.getItem("role")=="student")
-		 {
-			this.props.history.push('/student/login');//跳转至登录页
-		}
-		this.setState({roleSet : localStorage.getItem("roleSet")})
+		// if(!localStorage.getItem("userName")||!localStorage.getItem("role")=="student")
+		//  {
+		// 	this.props.history.push('/student/login');//跳转至登录页
+		// }
+		// this.setState({roleSet : localStorage.getItem("roleSet")})
 	}
 
 
@@ -41,9 +42,13 @@ class Student extends React.Component {
 					{/* 在这里写菜单 
 				       <Link>是跳转的路径 
 					 */}
-				<SubMenu key="personal_center" title={<span><Icon type="user" /><span>个人中心</span></span>}>
+				<SubMenu key="personal_center" title={<span><Icon type="user" /><span>个人中心aa</span></span>}>
 				    <Menu.Item key="change_password"><Link to="/student/personal_center/change_password">修改密码</Link></Menu.Item>
 				</SubMenu>
+				<Menu.Item key="1">
+            <Icon type="pie-chart" />
+            <span><Link to="/student/my_class">我的班级</Link></span>
+          </Menu.Item>
 				</Menu>
 				</div>         
 			</div>
@@ -55,7 +60,7 @@ class Student extends React.Component {
 					path参数是拦截的路径， component是显示组件 且只能是/student的子路径 
 					记得导入组件
 					 */}
-          
+          <Route path="/student/my_class" component={MyClass}/>
 			</div>
 			</div>
 			</div>
